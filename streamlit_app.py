d35f177090aae4821e7c21e82bebccfdb380d7f4
eltonhcchan <elton.chan@yahoo.com>
2024-03-31 16:50:38 +0800

    add WFS request

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,37 @@
 import streamlit as st 
+import geopandas as gpd
+import requests
+import geojson
+import pygwalker as pyg
+from pyproj import CRS
+from owslib.wfs import WebFeatureService
 
-st.title("Testing") 
-st.subheader("Always try") 
-st.write("This is my first try using Streamlit and its cloud community. ✨ ")
+url = "https://portal.csdi.gov.hk/server/services/common/cedd_rcd_1640249280153_17521/MapServer/WFSServer"
+
+query_params = dict(
+    service="WFS",
+    version="2.0.0",
+    request="GetFeature",
+    typeName="esri:Incident",
+    outputFormat="GEOJSON",
+)
+
+def load_data(nrows):
+    # Fetch data from WFS using requests
+    query_params['resultREcordCount'] = nrows
+    r = requests.get(url, params=query_params)
+    data = gpd.GeoDataFrame.from_features(geojson.loads(r.content), crs="EPSG:4326") 
+    return data
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+data = load_data(500)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text('Loading data...done!')
+
+st.title("Connect to CSDI") 
+st.subheader("Find slope data") 
+st.write("Show it, show it✨ ")
+
+st.write(data)

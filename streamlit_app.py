69431ee2b90b8402b3adbf7e423e83e77fbe372f
eltonhcchan <elton.chan@yahoo.com>
2024-04-03 17:48:30 +0800

    modified

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,61 @@
-import streamlit as st
-import leafmap.foliumap as leafmap
+import streamlit as st 
+import geopandas as gpd
+import requests
+import geojson
+from pygwalker.api.streamlit import StreamlitRenderer, init_streamlit_comm
+from pyproj import CRS
+from owslib.wfs import WebFeatureService
 
-def app():
-    st.title("Searching Basemaps")
-    st.markdown(
-    "This app is a demonstration of searching and loading basemaps from [xyzservices](https://github.com/geopandas/xyzservices) and [Quick Map Services (QMS)](https://github.com/nextgis/quickmapservices). Selecting from 1000+ basemaps with a few clicks."
-    )
-    with st.expander("See demo"):
-        st.image("https://i.imgur.com/0SkUhZh.gif")
-    row1_col1, row1_col2 = st.columns([3, 1])
-    width = 800
-    height = 600
-    tiles = None
-    with row1_col2:
-        checkbox = st.checkbox("Search Quick Map Services (QMS)")
-        keyword = st.text_input("Enter a keyword to search and press Enter:")
-        empty = st.empty()
-        if keyword:
-            options = leafmap.search_xyz_services(keyword=keyword)
-            if checkbox:
-                qms = leafmap.search_qms(keyword=keyword)
-                if qms is not None:
-                    options = options + qms
-            tiles = empty.multiselect(
-                "Select XYZ tiles to add to the map:", options)
-    with row1_col1:
-        m = leafmap.Map()
-        if tiles is not None:
-            for tile in tiles:
-                m.add_xyz_service(tile)
-        m.to_streamlit(width, height)
+url = "https://portal.csdi.gov.hk/server/services/common/cedd_rcd_1640249280153_17521/MapServer/WFSServer"
+
+query_params = dict(
+    service="WFS",
+    version="2.0.0",
+    request="GetFeature",
+    typeName="esri:Incident",
+    outputFormat="GEOJSON",
+)
+
+# Adjust the width of the Streamlit page
+st.set_page_config(
+    page_title="Showing landslide data from cSDI",
+    layout="wide"
+)
+
+def load_data(nrows):
+    # Fetch data from WFS using requests
+    query_params['resultRecordCount'] = nrows
+    r = requests.get(url, params=query_params)
+    data = gpd.GeoDataFrame.from_features(geojson.loads(r.content), crs="EPSG:4326") 
+    return data
+
+# Establish communication between pygwalker and streamlit
+init_streamlit_comm()
+ 
+# Add a title
+st.title("Loading Pygwalker In Streamlit")
+
+# Get an instance of pygwalker's renderer. You should cache this instance to effectively prevent the growth of in-process memory.
+@st.cache_resource
+def get_pyg_renderer() -> "StreamlitRenderer":
+   # Create a text element and let the reader know the data is loading.
+    st.text('Loading data...')
+    
+    df = load_data(5000)
+    # When you need to publish your app to the public, you should set the debug parameter to False to prevent other users from writing to your chart configuration file.
+    # Notify the reader that the data was successfully loaded.
+    st.text('Loading data...done!')
+    return StreamlitRenderer(df, spec="./gw_config.json", debug=False)
+ 
+renderer = get_pyg_renderer()
+
+tab1, tab2 = st.tabs(
+    ["graphic walker", "data profiling"]
+)
+
+with tab1:
+    # Render your data exploration interface. Developers can use it to build charts by drag and drop.
+    renderer.render_explore()
+
+with tab2:
+    renderer.explorer(default_tab="data")
